e56996c324e810365b84b4e0cf61cccd1f989d04
Arella Gussow <arellaeg@gmail.com>
2018-06-05 02:53:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function shout(string){
   return string.toUpperCase()
 }
+
+function whispering(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log(string.toUpperCase);
+}
